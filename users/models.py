7cca5c84728f47f2e613775a7dabe07655b3e8e3
mane <tomevmarjan33@gmail.com>
2024-10-06 02:44:20 +0200

    renamed tables

--- users/models.py
+++ users/models.py
@@ -7,11 +7,13 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=255)
     username = models.CharField(max_length=255, unique=True)
     email = models.EmailField(unique=True, max_length=255)
-    favorite_categories = models.ManyToManyField('Category', related_name='users', blank=True)
+    favorite_categories = models.ManyToManyField('Category', through='UserCategory', related_name='users', blank=True)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
-    followers = models.ManyToManyField('self', related_name='followed_by_this_user', symmetrical=False)
-    following = models.ManyToManyField('self', related_name='followers_of_this_user', symmetrical=False)
+    followers = models.ManyToManyField('self', through='UserFollowers', related_name='followed_by_this_user',
+                                       symmetrical=False)
+    following = models.ManyToManyField('self', through='UserFollowing', related_name='followers_of_this_user',
+                                       symmetrical=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
@@ -19,12 +21,42 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+    class Meta:
+        db_table = 'user'
+
+
+class UserCategory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'user_favorite_categories'
+
+
+class UserFollowers(models.Model):
+    user_from = models.ForeignKey(User, related_name='following_relationships', on_delete=models.CASCADE)
+    user_to = models.ForeignKey(User, related_name='follower_relationships', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'user_followers'
+
+
+class UserFollowing(models.Model):
+    user_from = models.ForeignKey(User, related_name='follower_relationships_set', on_delete=models.CASCADE)
+    user_to = models.ForeignKey(User, related_name='following_relationships_set', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'user_following'
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        db_table = 'category'
 
 
 class CodeSnippet(models.Model):
@@ -37,3 +69,6 @@ class CodeSnippet(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        db_table = 'code_snippet'

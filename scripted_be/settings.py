47a6e1b9dc8f806ae52b418c3150739d8d5ef7d8
mane <tomevmarjan33@gmail.com>
2024-10-10 02:35:42 +0200

    - corsheaders implemented

--- scripted_be/settings.py
+++ scripted_be/settings.py
@@ -12,9 +12,14 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+# Allowed hosts
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
+
+# Cors settings
+CORS_ALLOWED_ORIGINS = env('CORS_ALLOWED_ORIGINS').split(',')
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -29,6 +34,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
